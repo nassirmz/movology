821041f5b5e3c49cc3dc6b9e39892ba7b83bc238
root <root@ip-172-31-3-203.us-west-1.compute.internal>
2016-08-25 05:35:13 +0000

    Send name

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var qs = require('qs');
+var querystring = require('querystring');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
 var port = process.env.PORT || 3001;
 
@@ -12,8 +14,16 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function (req, res) {
-  console.log(req.query);
-  res.sendStatus(200);
+  var query = querystring.unescape(req.query.q);
+  query = query.replace(/\'/g, "");
+  query = qs.parse(query);
+  console.log(query.q);
+  switch(Object.keys(query.q)[0]) {
+   case 'Name':
+    res.send('Nassir Zeinu');
+   default:
+    res.sendStatus(200);
+  }
 });
 
 
