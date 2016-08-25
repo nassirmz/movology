ded6eac8c287f8b351ae5f718f8e2d11f6732978
nassir <nassirmz@gmail.com>
2016-08-24 21:42:54 -0700

    Add morgan logging

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var morgan = require('morgan');
 var app = express();
 
 app.use(bodyParser.json());
-
+app.use(morgan('dev'));
 var port = process.env.PORT || 3001;
 
 app.get('/', function (req, res) {
