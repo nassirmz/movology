8649faaf2594201af5e263b7bd8d1e73c856ae87
root <root@ip-172-31-3-203.us-west-1.compute.internal>
2016-08-25 06:09:01 +0000

    Solve puzzle

--- server/post.js
+++ server/post.js
@@ -27,6 +27,12 @@ module.exports = function (req, res) {
     case 'Status':
       res.send('US citizen');
       break;
+    case 'Puzzle':
+      console.log(req.query);
+      var puzzle = new Buffer(req.query.puzzle, 'base64');
+      var ans = eval(puzzle.toString());
+      res.send(ans + '');
+      break;
     default:
       res.sendStatus(200);
   }
