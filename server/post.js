91e14665ed0e8728dd033408bb18fedae28caede
nassir <nassirmz@gmail.com>
2016-08-24 22:49:21 -0700

    Add break statements

--- server/post.js
+++ server/post.js
@@ -8,20 +8,26 @@ module.exports = function (req, res) {
   switch(Object.keys(query.q)[0]) {
     case 'Name':
       res.send('Nassir Zeinu');
+      break;
     case 'Email':
       res.send('nassirmz@gmail.com');
+      break;
     case 'Years':
       res.send('1');
+      break;
     case 'Position':
       res.send('Full Stack Engineer');
+      break;
     case 'Phone':
       res.send('323-345-8161');
+      break;
     case 'Referrer':
       res.send('indeed.com');
+      break;
     case 'Status':
-      res.send('US citizen')
-
-   default:
-    res.sendStatus(200);
+      res.send('US citizen');
+      break;
+    default:
+      res.sendStatus(200);
   }
 }
